56e85d5657ce21c026f2c801090bfd1c07a92207
sakthivel.chandru <sakthivel.chandru@SWPC164.gswvss.com>
2023-02-22 19:42:21 +0530

    stage [22.02.2023]

--- bdd.swordgin.cucumber_update/src/test/java/runner/testRunner.java
+++ bdd.swordgin.cucumber_update/src/test/java/runner/testRunner.java
@@ -11,11 +11,11 @@ import io.cucumber.junit.CucumberOptions;
 	features = ".//Feature",
 	glue = "stepdefinitions",
 	dryRun = false,
-//	tags = ("@HRJobPosition"),
-	tags = ("@1Login or @2smoke or @Designation or @AddEmployee or @CreateProject or @Announcement or @Feedback or @SelfReport or @Report or @LeaveReport" ),
+	tags = ("@SelfReport"),
+//	tags = ("@1Login or @2smoke or @Designation or @AddEmployee or @CreateProject or @Announcement or @Feedback or @SelfReport or @Report or @LeaveReport" ),
 	
-//	plugin = {"pretty","html:test-output"}
-	plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+	plugin = {"pretty","html:test-output"}
+//	plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 	)
 
 public class testRunner  {
